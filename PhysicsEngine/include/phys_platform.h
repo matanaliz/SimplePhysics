75eb6db65fb2c5f0849bd2ff3f9902d8be818dbf
matanaliz <ol.kaspruk@gmail.com>
2016-03-21 16:43:47 +0200

    Added constexpr and noexept handling for VS

--- PhysicsEngine/include/phys_platform.h
+++ PhysicsEngine/include/phys_platform.h
@@ -7,6 +7,24 @@
 	#define PHYS_API __declspec(dllimport)
 #endif
 
+#ifdef _MSC_VER
+
+// Implementation is intentionally forbidding macroized keywords
+#define _ALLOW_KEYWORD_MACROS 
+// No MSVC does constexpr fully yet
+#pragma push_macro("constexpr")
+#define constexpr 
+
+// MSVC 2013 workarounds
+#if _MSC_VER <= 1800
+
+#pragma push_macro("noexcept")
+#define noexcept  
+
+#endif // _MSC_VER <= 1800
+
+#endif // _MSC_VER
+
 // TODO Add linux exports
 
 #endif //PHYS_PLATFORM_H
